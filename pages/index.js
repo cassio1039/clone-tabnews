bf007c2ffa0ed9b7240042210af8b436e66305de
Cássio Martins <97923417+cassio1039@users.noreply.github.com>
2024-12-11 16:10:01 +0000

    adicionei um texto novo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Página de teste para exercício prático do curso</h1>
+    return <h1>Página de teste para exercício prático do curso novo</h1>
 }
 
 export default Home;

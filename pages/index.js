3f8baefcb38d273455c181443feda300daaa05b3
Cássio Martins <97923417+cassio1039@users.noreply.github.com>
2024-12-17 18:18:37 +0000

    habilitando os script lint:check e lint:fix para checar e arrumar os códigos automaticamente

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-function Home(){
-    
-    return <h1>Página de teste para exercício prático do curso novo</h1>
-    
+function Home() {
+  return <h1>Página de teste para exercício prático do curso novo</h1>;
+}
+function teste() {
+  console.log("teste")
+}
+function teste2(){
+  console.log("identação errada")
 }
 
 export default Home;

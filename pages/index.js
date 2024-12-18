e0320b4206b047a23dd4719b821746258057b311
Cássio Martins <97923417+cassio1039@users.noreply.github.com>
2024-12-18 15:22:39 +0000

    adicionando o .prettierignore

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ function Home() {
   return <h1>Página de teste para exercício prático do curso novo</h1>;
 }
 function teste() {
-  console.log("teste")
+  console.log("teste");
 }
-function teste2(){
-  console.log("identação errada")
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
